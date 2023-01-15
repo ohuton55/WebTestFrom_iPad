26e29739f42c723895b8959e40619676f856e4ed
ohuton55 <tttt.tttt.no@gmail.com>
2023-01-16 08:36:20 +0900

    Try using with TEMPLATES.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.Template import loader
+
+from .models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. you're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #template = loader.get_template('polls/index.html')
+    context = {
+      'latest_question_list': latest_question_list,
+    }  
+    return render(request, 'polls/index.html', context)
+    #return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." %question_id)
